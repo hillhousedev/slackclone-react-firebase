44956b3484a9707fe0854df7e3a4a75cc9edda73
hillhousedev <adesemoyeolusola@gmail.com>
2020-12-06 21:06:32 +0100

    Finishing sidebar option

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -6,6 +6,19 @@ import CreateIcon from '@material-ui/icons/Create';
 import SidebarOption from "./SidebarOption";
 import InsertCommentIcon from '@material-ui/icons/InsertComment';
 
+import InboxIcon from '@material-ui/icons/Inbox';
+
+import DraftsIcon from '@material-ui/icons/Drafts';
+import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
+import PeopleAltIcon from '@material-ui/icons/PeopleAlt';
+
+
+import AppsIcon from '@material-ui/icons/Apps';
+import FileCopyIcon from '@material-ui/icons/FileCopy';
+import ExpandLessIcon from '@material-ui/icons/ExpandLess';
+import AddIcon from '@material-ui/icons/Add';
+
+
 function Sidebar() {
     return (
         <div className="sidebar">
@@ -20,7 +33,20 @@ function Sidebar() {
                 <CreateIcon />
             </div>
             <SidebarOption Icon={InsertCommentIcon} title="Threads" />
-            <SidebarOption title="Youtube" />
+            <SidebarOption title="Mention & reactions" />
+            <SidebarOption Icon={DraftsIcon} title="Saved items" />
+            <SidebarOption Icon={BookmarkBorderIcon} title="Channel browser" />
+            <SidebarOption Icon={PeopleAltIcon} title="People & user groups" />
+            <SidebarOption Icon={AppsIcon} title="Apps" />
+            <SidebarOption Icon={FileCopyIcon} title="File browser" />
+            <SidebarOption Icon={ExpandLessIcon} title="Show less" />
+
+            <hr />
+            <SidebarOption Icon={ExpandLessIcon} title="Channels" />
+
+            <hr />
+            <SidebarOption Icon={AddIcon} title="Add Channel" />
+
         </div>
     )
 }
