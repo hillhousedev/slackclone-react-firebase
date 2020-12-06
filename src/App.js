51350a6e893c58187fdc250084cc98875985ff32
hillhousedev <adesemoyeolusola@gmail.com>
2020-12-06 06:28:29 +0100

    UI Elements added to header components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Header from "./Header"
 
 function App() {
     return ( 
 
         //BEM naming convention
     <div className = "App" >
+    <Header />
         {/* Header */}
         {/** Sidebar */} 
         {/** React-Router  => Chat Screen */}
---
+++ src/Header.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Avatar } from "@material-ui/core";
+import AccessTimeIcon from '@material-ui/icons/AccessTime';
+import SearchIcon from '@material-ui/icons/Search';
+
+import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
+
+function Header() {
+    return (
+        <div className="header"> 
+            <div className="header__left">
+                {/** Avatars for logged in user */}
+                <Avatar className="header__avatar" alt='olusola' src='' />
+                <AccessTimeIcon />
+                
+
+                {/** Time icon */}
+            </div>
+                <div className="header__search">
+                <SearchIcon />
+                    {/** Search icon */}
+                    {/** input */}
+                    <input placeholder="Search through this slack channel" />
+
+
+                </div>
+                <div className="header__right">
+                    {/** help icon */}
+                <HelpOutlineIcon />
+                
+                </div>
+            
+        </div>
+    )
+}
+
+export default Header
