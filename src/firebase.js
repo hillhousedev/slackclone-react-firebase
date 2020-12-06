b43c1ac0424bbb0814ba27e45296adf66bbca8a1
hillhousedev <adesemoyeolusola@gmail.com>
2020-12-06 21:24:43 +0100

    Installed firebase module

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAdh8yLF9GqMa3WBK1ZUpesQHvKRHoiEps",
+    authDomain: "slack-clone-with-reactfirebase.firebaseapp.com",
+    projectId: "slack-clone-with-reactfirebase",
+    storageBucket: "slack-clone-with-reactfirebase.appspot.com",
+    messagingSenderId: "259684472670",
+    appId: "1:259684472670:web:6e7f4d0ffcdb1d8bfba1cf"
+};
