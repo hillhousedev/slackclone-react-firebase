0ff67b092ac0bbff3aea18d79b0752c76aa980bb
hillhousedev <adesemoyeolusola@gmail.com>
2020-12-07 12:12:51 +0100

    Created router and added chats

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
 import './App.css';
 import Header from "./Header"
 import Sidebar from "./Sidebar"
+import { BrowserRouter as Router, Switch, Route } 
+from "react-router-dom";
 
 function App() {
     return ( 
 
         //BEM naming convention
     <div className = "App" >
-    <Header />
-    <div className="app_body">
-        <Sidebar />
-                {/** Sidebar */}
-                {/** React-Router  => Chat Screen */}
-    </div>
+        <Router>
+
         
+        <Header />
+        <div className="app_body">
+            <Sidebar />
+            <Switch>
+                <Route path="/room/:roomId">
+                    <h1>CHAT SCREEN</h1>
+                    {/* <Chat /> */}
+                </Route>
+                <Route path="/">
+                    <h1>Welcome</h1>
+                </Route>
+            </Switch>
+        
+                    {/** React-Router  => Chat Screen */}
+        </div>
+    </Router>
     </div>
 
     );
