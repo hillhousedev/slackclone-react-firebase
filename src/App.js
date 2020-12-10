4b455bdd7aa00947a82b198b4e2f5e1c32274d45
hillhousedev <adesemoyeolusola@gmail.com>
2020-12-10 15:16:17 +0100

    Message fully styled

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import React, { useState } from "react";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
 import Chat from "./Chat";
@@ -6,31 +7,37 @@ import { BrowserRouter as Router, Switch, Route }
 from "react-router-dom";
 
 function App() {
+
+    const [user, setUser] = useState(null);
+
     return ( 
 
         //BEM naming convention
     <div className = "App" >
         <Router>
+            {!user ? (
+                <h1>LOGIN page</h1>
+            ): (
+                 <>
+                    <Header />
+                        <div className="app_body">
+                            <Sidebar />
+                            <Switch>
+                                <Route path="/room/:roomId">
 
-        
-        <Header />
-        <div className="app_body">
-            <Sidebar />
-            <Switch>
-                <Route path="/room/:roomId">
-                    
-                    <Chat />
-                </Route>
-                <Route path="/">
-                    <h1>Welcome</h1>
-                </Route>
-            </Switch>
-        
-                    {/** React-Router  => Chat Screen */}
-        </div>
-    </Router>
+                                    <Chat />
+                                </Route>
+                                <Route path="/">
+                                    <h1>Welcome</h1>
+                                </Route>
+                            </Switch>
+
+                            {/** React-Router  => Chat Screen */}
+                        </div>
+                        </>
+                    )}
+            </Router>
     </div>
-
     );
 }
 
