9fb03541a7b653dfd0a09a11ab89e3d936c13c72
hillhousedev <adesemoyeolusola@gmail.com>
2020-12-10 15:47:24 +0100

    Styling login component

--- src/Login.js
+++ src/Login.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./Login.css";
+import { Button } from "@material-ui/core";
 
 function Login() {
     return (
         <div className="login">
             <div className="login__container">
                 <img src="https://cdn.mos.cms.futurecdn.net/SDDw7CnuoUGax6x9mTo7dd.jpg" alt=""/>
+                <h1>Sign in to hilldev slack clone</h1>
+                <p>Movebizonline.com</p>
+                <Button>Sign In with Google</Button>
             </div>
             
         </div>
